8524413c83c0c88cf40b31f25ba5c7feb481b1bf
IAMHUDA <hudamiftahul362@gmail.com>
2023-07-02 19:00:52 +0700

    menambahkan method insert

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -27,3 +27,13 @@ public:
 	{
 		ROOT = NULL; //initializing Root to null
 	}
+	void insert(string element) // insert a node in the binary search tree
+	{
+		node* NewNode = new node(element, NULL, NULL); // allocate memory for the new node
+		NewNode->info = element; //Assign value to the data field of the new node
+		NewNode->leftchild = NULL; //Make the left child of the new node point to null
+		NewNode->rightchild = NULL; //Make the right child of the new node point to NULL
+
+		node* parent = NULL;
+		node* currentNode = NULL;
+		search(element, parent, currentNode); //Locate the node which will be the parent of the node to be inserted
